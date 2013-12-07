5a55cf253b69378a788f592019e5d7cba32ab214
Pip Taylor <pip@evilgeek.co.uk>
2013-12-07 11:02:47 +0000

    Exercise 1-15.
    Rewriting farenheit to celcius program to use a function for conversion.

--- temperature_f_to_c.c
+++ temperature_f_to_c.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+float farenheit_to_celsius(float fahr);
+
 /* print Fahrenheit-Celsius table
  * for fahr = 0, 20, ..., 300 */
 int main() {
@@ -13,8 +15,11 @@ int main() {
   fahr = lower;
   printf("Farenheit Celsius\n");
   while (fahr <= upper) {
-    celsius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%9.0f %7.1f\n", fahr, celsius);
+    printf("%9.0f %7.1f\n", fahr, farenheit_to_celsius(fahr));
     fahr = fahr + step;
   }
+}
+
+float farenheit_to_celsius(float fahr) {
+  return (5.0 / 9.0) * (fahr - 32.0);
 }
