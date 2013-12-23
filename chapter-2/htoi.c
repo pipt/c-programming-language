3dcad4008b5a9a7a8ce6647de7e8bc8483d49061
Pip Taylor <pip@evilgeek.co.uk>
2013-12-23 10:37:07 +0000

    Replace charval with a much neater implementation.
    Got rid of the huge switch statement, now use a char array and look up the
    index of the given character.

--- chapter-2/htoi.c
+++ chapter-2/htoi.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 
 #define MAXLINE 1000
 
 long htoi(char s[]);
 int starts_with_0x(char s[]);
 int charval(char c);
+
+const char HEX_ARRAY[] = "0123456789ABCDEF";
 
 int main() {
   int c, i;
@@ -42,62 +45,9 @@ long htoi(char s[]) {
 }
 
 int charval(char c) {
-  switch(c) {
-    case '0':
-      return 0;
-      break;
-    case '1':
-      return 1;
-      break;
-    case '2':
-      return 2;
-      break;
-    case '3':
-      return 3;
-      break;
-    case '4':
-      return 4;
-      break;
-    case '5':
-      return 5;
-      break;
-    case '6':
-      return 6;
-      break;
-    case '7':
-      return 7;
-      break;
-    case '8':
-      return 8;
-      break;
-    case '9':
-      return 9;
-      break;
-    case 'a':
-    case 'A':
-      return 10;
-      break;
-    case 'b':
-    case 'B':
-      return 11;
-      break;
-    case 'c':
-    case 'C':
-      return 12;
-      break;
-    case 'd':
-    case 'D':
-      return 13;
-      break;
-    case 'e':
-    case 'E':
-      return 14;
-      break;
-    case 'f':
-    case 'F':
-      return 15;
-      break;
-  }
+  for (int i = 0; i < 16; ++i)
+    if (HEX_ARRAY[i] == toupper(c))
+      return i;
   return 0;
 }
 
