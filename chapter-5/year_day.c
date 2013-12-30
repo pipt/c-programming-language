dd83db1fe78aff8c7f1f036b29e1f406eb550979
Pip Taylor <pip@evilgeek.co.uk>
2013-12-30 17:43:03 +0000

    Add error handling to year day functions.
    Exercise 5-8.

--- chapter-5/year_day.c
+++ chapter-5/year_day.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void month_day(int year, int yearday, int *pmonth, int *pday);
-int day_of_year(int year, int month, int day);
+void month_day(
+    int year, int yearday, int *pmonth, int *pday, int *err_code, char **err_str
+    );
+int day_of_year(int year, int month, int day, int *err_code, char **err_str);
 
 int main(int argc, char **argv) {
+  int err_code;
+  char *err_str;
   if (argc == 3) {
+    int year = atoi(argv[1]);
+    int yearday = atoi(argv[2]);
     int month;
     int day;
-    month_day(atoi(argv[1]), atoi(argv[2]), &month, &day);
-    printf("%d %d\n", month, day);
+    month_day(year, yearday, &month, &day, &err_code, &err_str);
+    if (err_code == 0)
+      printf("%d %d\n", month, day);
+    else
+      printf("%s\n", err_str);
   } else if (argc == 4) {
-    printf("%d\n", day_of_year(atoi(argv[1]), atoi(argv[2]), atoi(argv[3])));
+    int year = atoi(argv[1]);
+    int month = atoi(argv[2]);
+    int day = atoi(argv[3]);
+    int result = day_of_year(year, month, day, &err_code, &err_str);
+    if (err_code == 0)
+      printf("%d\n", result);
+    else
+      printf("%s\n", err_str);
   }
 }
 
@@ -20,19 +36,68 @@ static char daytab[2][13] = {
   {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
 
-int day_of_year(int year, int month, int day) {
+int day_of_year(int year, int month, int day, int *err_code, char **err_str) {
   int i, leap;
+  *err_code = 0;
+
+  if (year < 0) {
+    *err_code = -1;
+    *err_str = "error: year can't be negative";
+    return -1;
+  }
+  if (month < 1) {
+    *err_code = -1;
+    *err_str = "error: month can't be negative or zero";
+    return -1;
+  }
+  if (day < 1) {
+    *err_code = -1;
+    *err_str = "error: day can't be negative or zero";
+    return -1;
+  }
+  if (month > 12) {
+    *err_code = -1;
+    *err_str = "error: month can't be greater than 12";
+    return -1;
+  }
 
   leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
+
+  if (day > daytab[leap][month]) {
+    *err_code = -1;
+    *err_str = "error: day of month too big";
+    return -1;
+  }
   for (i = 1; i < month; i++)
     day += daytab[leap][i];
   return day;
 }
 
-void month_day(int year, int yearday, int *pmonth, int *pday) {
+void month_day(
+    int year, int yearday, int *pmonth, int *pday, int *err_code, char **err_str
+    ) {
   int i, leap;
+  *err_code = 0;
+
+  if (year < 0) {
+    *err_code = -1;
+    *err_str = "error: year can't be negative";
+    return;
+  }
+  if (yearday < 0) {
+    *err_code = -1;
+    *err_str = "error: year day can't be negative";
+    return;
+  }
 
   leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
+
+  if ((leap && yearday > 366) || (!leap && yearday > 365)) {
+    *err_code = -1;
+    *err_str = "error: year day too big";
+    return;
+  }
+
   for (i = 1; yearday > daytab[leap][i]; i++)
     yearday -= daytab[leap][i];
   *pmonth = i;
--- spec/chapter-5/year_day_spec.rb
+++ spec/chapter-5/year_day_spec.rb
@@ -15,6 +15,26 @@ describe "year and day to month and day" do
       expect(run(date.year, date.yday)).to eq("#{date.mon} #{date.day}")
     end
   end
+
+  it "reports an error if the year is negative" do
+    expect(run(-1, 1)).to eq("error: year can't be negative")
+  end
+
+  it "reports and error if the year day is negative" do
+    expect(run(1, -1)).to eq("error: year day can't be negative")
+  end
+
+  it "reports an error if the year is not a leap year and the year day > 365" do
+    expect(run(2001, 366)).to eq("error: year day too big")
+  end
+
+  it "reports an error if the year is a leap year and the year day > 366" do
+    expect(run(2004, 367)).to eq("error: year day too big")
+  end
+
+  it "returns the correct month and day if a leap year and day 366" do
+    expect(run(2004, 366)).to eq("12 31")
+  end
 end
 
 describe "year, month and day to year day" do
@@ -27,5 +47,37 @@ describe "year, month and day to year day" do
       date = random_date
       expect(run(date.year, date.mon, date.day)).to eq(date.yday.to_s)
     end
+  end
+
+  it "reports an error if the year is negative" do
+    expect(run(-1, 1, 1)).to eq("error: year can't be negative")
+  end
+
+  it "reports an error if the month is negative" do
+    expect(run(1, -1, 1)).to eq("error: month can't be negative or zero")
+  end
+
+  it "reports an error if the day is negative" do
+    expect(run(1, 1, -1)).to eq("error: day can't be negative or zero")
+  end
+
+  it "reports an error if the month is > 12" do
+    expect(run(1, 14, 1)).to eq("error: month can't be greater than 12")
+  end
+
+  it "reports an error if the month has 28 days in and the day > 28" do
+    expect(run(2001, 2, 29)).to eq("error: day of month too big")
+  end
+
+  it "reports an error if the month has 29 days in and the day > 29" do
+    expect(run(2004, 2, 30)).to eq("error: day of month too big")
+  end
+
+  it "reports an error if the month has 30 days in and the day > 30" do
+    expect(run(2001, 4, 31)).to eq("error: day of month too big")
+  end
+
+  it "reports an error if the month has 31 days in and the day > 31" do
+    expect(run(2004, 12, 32)).to eq("error: day of month too big")
   end
 end
