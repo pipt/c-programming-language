56b2916293ebe07008089dbbae7fa2ed7a0f406e
Pip Taylor <pip@evilgeek.co.uk>
2013-12-31 20:55:53 +0000

    Change keywords program to use pointers.

--- chapter-6/keywords.c
+++ chapter-6/keywords.c
@@ -44,38 +44,38 @@ struct key {
 };
 
 int getword(char *, int);
-int binsearch(char *, struct key *, int);
+struct key *binsearch(char *, struct key *, int);
 
 int main() {
-  int n;
   char word[MAXWORD];
+  struct key *p;
 
   while (getword(word, MAXWORD) != EOF)
     if (isalpha(word[0]))
-      if ((n = binsearch(word, keytab, NKEYS)) >= 0)
-        keytab[n].count++;
-  for (n = 0; n < NKEYS; n++)
-    if (keytab[n].count > 0)
-      printf("%4d %s\n", keytab[n].count, keytab[n].word);
+      if ((p = binsearch(word, keytab, NKEYS)) != NULL)
+        p->count++;
+  for (p = keytab; p < keytab + NKEYS; p++)
+    if (p->count > 0)
+      printf("%4d %s\n", p->count, p->word);
   return 0;
 }
 
-int binsearch(char *word, struct key tab[], int n) {
+struct key *binsearch(char *word, struct key *tab, int n) {
   int cond;
-  int low, high, mid;
+  struct key *low = &tab[0];
+  struct key *high = &tab[n];
+  struct key *mid;
 
-  low = 0;
-  high = n - 1;
-  while (low <= high) {
-    mid = (low + high) / 2;
-    if ((cond = strcmp(word, tab[mid].word)) < 0)
-      high = mid - 1;
+  while (low < high) {
+    mid = low + (high - low) / 2;
+    if ((cond = strcmp(word, mid->word)) < 0)
+      high = mid;
     else if (cond > 0)
       low = mid + 1;
     else
       return mid;
   }
-  return -1;
+  return NULL;
 }
 
 int buf = -1;
